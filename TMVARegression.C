27c2a0dfd86a071b11ffb1ba4799040e9485c322
Andrew Carnes <carnesandnoble@gmail.com>
2016-08-19 11:12:27 +0200

    first commit

---
+++ TMVARegression.C
@@ -0,0 +1,193 @@
+// @(#)root/tmva $Id$
+/**********************************************************************************
+ * Project   : TMVA - a Root-integrated toolkit for multivariate data analysis    *
+ * Package   : TMVA                                                               *
+ * Root Macro: TMVARegression                                                     *
+ *                                                                                *
+ * This macro provides examples for the training and testing of the               *
+ * TMVA classifiers.                                                              *
+ *                                                                                *
+ * As input data is used a toy-MC sample consisting of four Gaussian-distributed  *
+ * and linearly correlated input variables.                                       *
+ *                                                                                *
+ * The methods to be used can be switched on and off by means of booleans, or     *
+ * via the prompt command, for example:                                           *
+ *                                                                                *
+ *    root -l TMVARegression.C\(\"LD,MLP\"\)                                      *
+ *                                                                                *
+ * (note that the backslashes are mandatory)                                      *
+ * If no method given, a default set is used.                                     *
+ *                                                                                *
+ * The output file "TMVAReg.root" can be analysed with the use of dedicated       *
+ * macros (simply say: root -l <macro.C>), which can be conveniently              *
+ * invoked through a GUI that will appear at the end of the run of this macro.    *
+ **********************************************************************************/
+
+#include <cstdlib>
+#include <iostream> 
+#include <map>
+#include <string>
+
+#include "TChain.h"
+#include "TFile.h"
+#include "TTree.h"
+#include "TString.h"
+#include "TObjString.h"
+#include "TSystem.h"
+#include "TROOT.h"
+
+#include "TMVA/Tools.h"
+#include "TMVA/Factory.h"
+#include "TMVA/TMVARegGui.h"
+
+
+using namespace TMVA;
+   
+void TMVARegression( TString myMethodList = "" ) 
+{
+   // The explicit loading of the shared libTMVA is done in TMVAlogon.C, defined in .rootrc
+   // if you use your private .rootrc, or run from a different directory, please copy the 
+   // corresponding lines from .rootrc
+
+   // methods to be processed can be given as an argument; use format:
+   //
+   // mylinux~> root -l TMVARegression.C\(\"myMethod1,myMethod2,myMethod3\"\)
+   //
+
+   //---------------------------------------------------------------
+   // This loads the library
+   TMVA::Tools::Instance();
+
+   // --------------------------------------------------------------------------------------------------
+
+   // --- Here the preparation phase begins
+
+   // Create a new root output file
+   TString outfileName( "TMVAMode3Reg.root" );
+   TFile* outputFile = TFile::Open( outfileName, "RECREATE" );
+
+   // Create the factory object. Later you can choose the methods
+   // whose performance you'd like to investigate. The factory will
+   // then run the performance analysis for you.
+   //
+   // The first argument is the base of the name of all the
+   // weightfiles in the directory weight/ 
+   //
+   // The second argument is the output file for the training results
+   // All TMVA output can be suppressed by removing the "!" (not) in 
+   // front of the "Silent" argument in the option string
+   TMVA::Factory *factory = new TMVA::Factory( "TMVARegression", outputFile, 
+                                               "!V:!Silent:Color:DrawProgressBar" );
+
+   // If you wish to modify default settings 
+   // (please check "src/Config.h" to see all available global options)
+   //    (TMVA::gConfig().GetVariablePlotting()).fTimesRMS = 8.0;
+   //    (TMVA::gConfig().GetIONames()).fWeightFileDir = "myWeightDirectory";
+
+   // Define the input variables that shall be used for the MVA training
+   // note that you may also use variable expressions, such as: "3*var1/var2*abs(var3)"
+   // [all types of expressions that can also be parsed by TTree::Draw( "expression" )]
+   factory->AddVariable( "Eta",    "Eta",    "", 'F' );
+   factory->AddVariable( "dPhi12", "dPhi12", "", 'F' );
+   factory->AddVariable( "dEta12", "dEta12", "", 'F' );
+   factory->AddVariable( "clct1",  "clct1",  "", 'F' );
+   factory->AddVariable( "clct2",  "clct2",  "", 'F' );
+
+   // Add the variable carrying the regression target
+   factory->AddTarget( "GenPt" ); 
+
+   // It is also possible to declare additional targets for multi-dimensional regression, ie:
+   // -- factory->AddTarget( "fvalue2" );
+   // BUT: this is currently ONLY implemented for MLP
+
+   // Read training and test data (see TMVAClassification for reading ASCII files)
+   // load the signal and background event samples from ROOT trees
+   TFile *input(0);
+   TString fname = "../ptrootfiles/Output_Trimmed_97p5_Mode3_100k.root";
+
+   if (!gSystem->AccessPathName( fname )) 
+      input = TFile::Open( fname ); // check if file in local directory exists
+   
+   if (!input) 
+   {
+      std::cout << "ERROR: could not open data file" << std::endl;
+      exit(1);
+   }
+
+   std::cout << "--- TMVARegression           : Using input file: " << input->GetName() << std::endl;
+
+   // --- Register the regression tree
+
+   TTree *regTree = (TTree*)input->Get("theNtuple");
+
+   // global event weights per tree (see below for setting event-wise weights)
+   Double_t regWeight  = 1.0;   
+
+   // You can add an arbitrary number of regression trees
+   factory->AddRegressionTree( regTree, regWeight );
+
+   // Apply additional cuts on the signal and background samples (can be different)
+   TCut mycut = ""; // for example: TCut mycut = "abs(var1)<0.5 && abs(var2-0.5)<1";
+
+   // tell the factory to use all remaining events in the trees after training for testing:
+   factory->PrepareTrainingAndTestTree( mycut, 
+                                         "nTrain_Regression=100000:nTest_Regression=0:SplitMode=Random:NormMode=NumEvents:!V" );
+   // factory->PrepareTrainingAndTestTree( mycut, 
+   //                                      "nTrain_Regression=0:nTest_Regression=0:SplitMode=Random:NormMode=NumEvents:!V" );
+
+   // If no numbers of events are given, half of the events in the tree are used 
+   // for training, and the other half for testing:
+   //    factory->PrepareTrainingAndTestTree( mycut, "SplitMode=random:!V" );  
+
+   // ---- Book MVA methods
+   //
+   // please lookup the various method configuration options in the corresponding cxx files, eg:
+   // src/MethoCuts.cxx, etc, or here: http://tmva.sourceforge.net/optionRef.html
+   // it is possible to preset ranges in the option string in which the cut optimisation should be done:
+   // "...:CutRangeMin[2]=-1:CutRangeMax[2]=1"...", where [2] is the third input variable
+
+   factory->BookMethod( TMVA::Types::kBDT, "BDTG",
+                           "!H:!V:NTrees=64::BoostType=Grad:Shrinkage=0.3:nCuts=99999:MaxDepth=4:MinNodeSize=0.001:NegWeightTreatment=IgnoreNegWeightsInTraining" );
+   // --------------------------------------------------------------------------------------------------
+
+   // ---- Now you can tell the factory to train, test, and evaluate the MVAs
+
+   // Train MVAs using the set of training events
+   factory->TrainAllMethods();
+
+   // ---- Evaluate all MVAs using the set of test events
+   factory->TestAllMethods();
+
+   // ----- Evaluate and compare performance of all configured MVAs
+   factory->EvaluateAllMethods();    
+
+   // --------------------------------------------------------------
+   
+   // Save the output
+   outputFile->Close();
+
+   std::cout << "==> Wrote root file: " << outputFile->GetName() << std::endl;
+   std::cout << "==> TMVARegression is done!" << std::endl;      
+
+
+   // --- Book the MVA methods
+
+   TString dir    = "weights/";
+   TString prefix = "TMVARegression";
+
+   delete factory;
+}
+
+int main( int argc, char** argv )
+{
+   // Select methods (don't look at this code - not of interest)
+   TString methodList; 
+   for (int i=1; i<argc; i++) {
+      TString regMethod(argv[i]);
+      if(regMethod=="-b" || regMethod=="--batch") continue;
+      if (!methodList.IsNull()) methodList += TString(","); 
+      methodList += regMethod;
+   }
+   TMVARegression(methodList);
+   return 0;
+}
---
+++ TMVARegressionApplicationCalo.C
@@ -0,0 +1,152 @@
+/**********************************************************************************
+ * Project   : TMVA - a Root-integrated toolkit for multivariate data analysis    *
+ * Package   : TMVA                                                               *
+ * Exectuable: TMVARegressionApplication                                          *
+ *                                                                                *
+ * This macro provides a simple example on how to use the trained regression MVAs *
+ * within an analysis module                                                      *
+ **********************************************************************************/
+
+#include <cstdlib>
+#include <vector>
+#include <iostream>
+#include <map>
+#include <string>
+
+#include "TFile.h"
+#include "TTree.h"
+#include "TString.h"
+#include "TSystem.h"
+#include "TROOT.h"
+#include "TStopwatch.h"
+#include "TMath.h"
+#include "TNtuple.h"
+
+#include "TMVA/Tools.h"
+#include "TMVA/Reader.h"
+
+using namespace TMVA;
+
+void TMVARegressionApplication( TString myMethodList = "" ) 
+{
+   //---------------------------------------------------------------
+   // This loads the library
+   TMVA::Tools::Instance();
+
+   std::cout << std::endl;
+   std::cout << "==> Start TMVARegressionApplication" << std::endl;
+
+   // --------------------------------------------------------------------------------------------------
+
+   // --- Create the Reader object
+
+   TMVA::Reader *reader = new TMVA::Reader( "!Color:!Silent" );    
+
+   // Create a set of variables and declare them to the reader
+   // - the variable names MUST corresponds in name and type to those given in the weight file(s) used
+   Float_t GenPt, Eta, dPhi12, dEta12, clct1, clct2;
+   reader->AddVariable( "Eta", &Eta );
+   reader->AddVariable( "dPhi12", &dPhi12 );
+   reader->AddVariable( "dEta12", &dEta12 );
+   reader->AddVariable( "clct1", &clct1 );
+   reader->AddVariable( "clct2", &clct2 );
+
+   // Spectator variables declared in the training have to be added to the reader, too
+   //reader->AddSpectator( "GenPt",  &GenPt );
+
+   // --- Book the MVA methods
+
+   TString dir    = "weights/";
+   TString prefix = "TMVARegression";
+
+   // Book method(s)
+   TString methodName = "BDTG method";
+   TString weightfile = dir + prefix + "_" + TString("BDTG") + ".weights.xml";
+   reader->BookMVA( methodName, weightfile ); 
+   
+   // Prepare input tree (this must be replaced by your data source)
+   // in this example, there is a toy tree with signal and one with background events
+   // we'll later on use only the "signal" events for the test in this example.
+   //   
+   TFile *input(0);
+   TString fname = "../ptrootfiles/Output_Trimmed_97p5_TEST_Mode3_100k.root";
+   if (!gSystem->AccessPathName( fname )) 
+   {
+      input = TFile::Open( fname ); // check if file in local directory exists
+   } 
+   if (!input) 
+   {
+      std::cout << "ERROR: could not open data file" << std::endl;
+      exit(1);
+   }
+   std::cout << "--- TMVARegressionApp        : Using input file: " << input->GetName() << std::endl;
+
+   // --- Event loop
+
+   // Prepare the tree
+   // - here the variable names have to corresponds to your tree
+   // - you can use the same variables as above which is slightly faster,
+   //   but of course you can use different ones and copy the values inside the event loop
+   //
+   TTree* theTree = (TTree*)input->Get("theNtuple");
+   std::cout << "--- Select signal sample" << std::endl;
+   theTree->SetBranchAddress( "Eta", &Eta );
+   theTree->SetBranchAddress( "dPhi12", &dPhi12 );
+   theTree->SetBranchAddress( "dEta12", &dEta12 );
+   theTree->SetBranchAddress( "clct1", &clct1 );
+   theTree->SetBranchAddress( "clct2", &clct2 );
+   theTree->SetBranchAddress( "GenPt", &GenPt );
+
+   TFile* outfile = new TFile("test_results_TMVA.root", "RECREATE");
+   outfile->cd();
+   TString ntupleVars("GenPt:BDTPt:Eta:dPhi12:dEta12:clct1:clct2");
+   TNtuple* n = new TNtuple("BDTresults", "BDTresults", ntupleVars);
+
+   std::cout << "--- Processing: " << theTree->GetEntries() << " events" << std::endl;
+   TStopwatch sw;
+   sw.Start();
+   for (Long64_t ievt=0; ievt<theTree->GetEntries();ievt++) 
+   {
+
+      //if (ievt%1000 == 0) std::cout << "--- ... Processing event: " << ievt << std::endl;
+      
+      theTree->GetEntry(ievt);
+
+      // Retrieve the MVA target values (regression outputs) and fill into histograms
+      // NOTE: EvaluateRegression(..) returns a vector for multi-target regression
+      Float_t val = (reader->EvaluateRegression( "BDTG method" ))[0];
+      std::vector<Float_t> y;
+      y.push_back(TMath::Abs(GenPt));
+      y.push_back(TMath::Abs(val));
+      y.push_back(dPhi12);
+      y.push_back(dEta12);
+      y.push_back(clct1);
+      y.push_back(clct2);
+      n->Fill(&y[0]);
+   }
+   sw.Stop();
+   std::cout << "--- End of event loop: "; sw.Print();
+
+   outfile->cd();
+   n->Write();
+   outfile->Close();
+   delete outfile;
+
+   delete reader;
+    
+   std::cout << "==> TMVARegressionApplication is done!" << std::endl << std::endl;
+}
+
+int main( int argc, char** argv )
+{
+   // Select methods (don't look at this code - not of interest)
+   TString methodList; 
+   for (int i=1; i<argc; i++) {
+      TString regMethod(argv[i]);
+      if(regMethod=="-b" || regMethod=="--batch") continue;
+      if (!methodList.IsNull()) methodList += TString(","); 
+      methodList += regMethod;
+   }
+   TMVARegressionApplication(methodList);
+   return 0;
+}
